5ab18e9527a9be7f3cc69d2a7743a28765cf0b8b
Nawasan Wisitsingkhon <arikato110011@gmail.com>
2023-04-15 13:13:15 +0700

    add new menus and change styles

--- components/RandomButton.tsx
+++ components/RandomButton.tsx
@@ -4,7 +4,7 @@ type RandomButtonType = {
     onClick: MouseEventHandler<HTMLButtonElement>
 }
 export default function RandomButton({ onClick }: RandomButtonType) {
-    return <button onClick={onClick} className="bg-zinc-100 py-2 px-4 rounded border border-zinc-300 my-3">
+    return <button onClick={onClick} className="bg-zinc-100 py-2 px-4 rounded border border-orange-300 my-3 shadow shadow-orange-200 hover:scale-90 duration-200">
         สุ่ม
     </button>
 }
--- components/ShowFood.tsx
+++ components/ShowFood.tsx
@@ -9,7 +9,7 @@ interface Food {
 const ShowFood: FC<Food> = ({ id, name, img, link }) => {
     return (
         <a target={'_blank'} href={link} rel="noreferrer">
-            <div className='text-orange-500 border border-zinc-200 rounded-lg p-3 shadow-md shadow-orange-200 text-center'>
+            <div className='bg-white text-orange-500 border border-orange-200 rounded-lg p-3 shadow-md shadow-orange-200 text-center'>
                 <div className="text-xl">{name}</div>
                 <img className='w-64 h-64 inline-block object-cover rounded border-2 border-zinc-200' src={img} alt="" />
                 {/* <Image className="w-64 h-64 object-cover rounded border-2 border-zinc-200" src={img} alt="food image" /> */}
